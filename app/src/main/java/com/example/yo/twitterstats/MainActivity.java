be451985d5c0ddd79f08e009ef20d6a59f09bd69
Pablo Hevia Viejo <UO251259@uniovi.es>
2018-01-08 16:53:30 +0100

    Carga userdata al hacer login

--- app/src/main/java/com/example/yo/twitterstats/MainActivity.java
+++ app/src/main/java/com/example/yo/twitterstats/MainActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.twitter.sdk.android.core.Twitter;
 import com.twitter.sdk.android.core.TwitterAuthToken;
 import com.twitter.sdk.android.core.TwitterCore;
@@ -13,6 +16,7 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.User;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
                 String userName = result.data.getUserName();
                 long userID = result.data.getId();
+
+                loadUserData(userID);
             }
 
             @Override
@@ -52,6 +58,30 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    private void loadUserData(long userID) {
+        new MyTwitterApiClient(session).getCustomService().show(userID)
+                .enqueue(new Callback<User>() {
+                    @Override
+                    public void success(Result<User> result) {
+                        ((TextView) findViewById(R.id.userData)).setText(
+                                        "Name: "          + result.data.name
+                                        +"\nScreen name: "+ result.data.screenName
+                                        +"\nLocation: "   + result.data.location
+                                        +"\nFollowing: "    + result.data.friendsCount
+                                        +"\nFollowers: "    + result.data.followersCount
+                        );
+                        Picasso.with(getBaseContext()).load(result.data.profileImageUrl).
+                                resize(250,250)
+                                .into((ImageView)findViewById(R.id.profilePic));
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        Log.e("Failed", exception.toString());
+                    }
+                });
     }
 
     @Override
