1f2c5b407bc48229340814e5b9f1a5c54bedb4c6
pabloHeviaV <pablohv12@gmail.com>
2018-01-11 21:29:21 +0100

    Creación de clase para obtener los datos de los seguidores

--- app/src/main/java/com/example/yo/twitterstats/MyTwitterApiClient.java
+++ app/src/main/java/com/example/yo/twitterstats/MyTwitterApiClient.java
@@ -1,10 +1,13 @@
 package com.example.yo.twitterstats;
 
+import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.TwitterApiClient;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.models.User;
 
+import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Response;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
@@ -13,12 +16,22 @@ public class MyTwitterApiClient extends TwitterApiClient {
         super(session);
     }
 
-    public GetUsersShowAPICustomService getCustomService() {
+    public GetUsersShowAPICustomService getUserCustomService() {
         return getService(GetUsersShowAPICustomService.class);
     }
+
+    public GetFollowersListAPICustomService getFollowersListCustomService(){
+        return getService(GetFollowersListAPICustomService.class);
+    }
+
 }
 
 interface GetUsersShowAPICustomService {
     @GET("/1.1/users/show.json")
     Call<User> show(@Query("user_id") long userId);
+}
+
+interface GetFollowersListAPICustomService {
+    @GET("/1.1/followers/list.json")
+    void list(@Query("user_id") long id, Callback<Response> cb);
 }
